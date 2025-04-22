0ebb343242263ad50714aa3a94af4cd64d214742
n-abumheasen <nimonimer99@gmail.com>
2025-04-22 15:51:05 +0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,33 +1,31 @@
 import argparse
-import os
-from gevent import monkey
+import sys
 
-# إعدادات البرامتر
-parser = argparse.ArgumentParser()
-parser.add_argument("--port", help="Port for debug server to listen on", default=4000)
-parser.add_argument(
-    "--profile", help="Enable flask_profiler profiling", action="store_true"
-)
-parser.add_argument(
-    "--disable-gevent",
-    help="Disable importing gevent and monkey patching",
-    action="store_false",
-)
-args = parser.parse_args()
+# استبدال argparse بتحديد المعاملات فقط إذا لم يكن الكود يتم تشغيله بواسطة gunicorn
+if 'gunicorn' not in sys.argv[0]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", help="Port for debug server to listen on", default=4000)
+    parser.add_argument(
+        "--profile", help="Enable flask_profiler profiling", action="store_true"
+    )
+    parser.add_argument(
+        "--disable-gevent",
+        help="Disable importing gevent and monkey patching",
+        action="store_false",
+    )
+    args = parser.parse_args()
+    
+    if args.disable_gevent:
+        print(" * Importing gevent and monkey patching. Use --disable-gevent to disable.")
+        from gevent import monkey
+        monkey.patch_all()
 
-# إذا كان --disable-gevent مفعلاً
-if args.disable_gevent:
-    print(" * Importing gevent and monkey patching. Use --disable-gevent to disable.")
-    monkey.patch_all()
-
-# استيراد التطبيق
+# Import not at top of file to allow gevent to monkey patch uninterrupted
 from CTFd import create_app
 
-# إنشاء التطبيق
 app = create_app()
 
-# إضافة دعم profiling إذا كان مفعلاً
-if args.profile:
+if 'gunicorn' not in sys.argv[0] and args.profile:
     from flask_debugtoolbar import DebugToolbarExtension
     import flask_profiler
 
@@ -45,12 +43,6 @@ if args.profile:
     toolbar.init_app(app)
     print(" * Flask profiling running at http://127.0.0.1:4000/flask-profiler/")
 
-# تحقق من إذا كنت تعمل مع gunicorn أو لا
+# يجب استخدام gunicorn لتشغيل التطبيق
 if __name__ == "__main__":
-    # إذا كانت البيئة تستخدم gunicorn، لا نحتاج إلى app.run
-    if os.environ.get("GUNICORN_CMD_ARGS"):
-        # تأكد من أن gunicorn يقوم بتشغيله
-        print("Running with gunicorn")
-    else:
-        # في حالة تشغيل التطبيق محليًا (أو غير gunicorn)
-        app.run(debug=True, threaded=True, host="127.0.0.1", port=args.port)
+    app.run(debug=True, threaded=True, host="127.0.0.1", port=args.port)
